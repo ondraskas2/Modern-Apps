a8b4fb5311c85660cc9addda1856705672346c7a
Vayun Mathur <69178052+vayun-mathur@users.noreply.github.com>
2026-07-16 11:07:16 -0700

    remove undeveloped apps

--- launcher/src/main/java/com/vayunmathur/launcher/search/AppDocument.kt
+++
@@ -1,7 +0,0 @@
-package com.vayunmathur.launcher.search
-
-data class AppDocument(
-    val id: String,
-    val name: String,
-    val packageName: String
-)
--- launcher/src/main/java/com/vayunmathur/launcher/search/CalendarDocument.kt
+++
@@ -1,9 +0,0 @@
-package com.vayunmathur.launcher.search
-
-data class CalendarDocument(
-    val id: String,
-    val title: String,
-    val date: String = "",
-    val location: String = "",
-    val eventId: Long = 0
-)
